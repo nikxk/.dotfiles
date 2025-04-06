9c1a4e41375fa524e4ff80bbd9efec286ab8135e
Nikhil Sethukumar <nsethukumar@ethz.ch>
2025-04-07 00:34:12 +0200

    Better organize aliases

--- .bash_aliases
+++ .bash_aliases
@@ -1,3 +1,21 @@
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	alias ls='ls --color=auto'
+	#alias dir='dir --color=auto'
+	#alias vdir='vdir --color=auto'
+
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
 # quickies
 alias v='nvim'
 alias g='gitui'
@@ -12,6 +30,10 @@ alias cd...='cd ../../..'
 alias cd....='cd ../../../..'
 alias cdd='cd ~/downloads'
 
+# For making and changing into a directory
+mkcd() {
+	mkdir -p "$1" && cd "$1"
+}
+
 # rare
 alias show_colors='for i in {0..255}; do  printf "\x1b[38;5;${i}mcolor%-5i\x1b[0m" $i ; if ! (( ($i + 1 ) % 8 )); then echo ; fi ; done'
-alias cdwin='cd /mnt/c/Users/Nikhil'
--- .fzf.bash
+++ .fzf.bash
@@ -4,10 +4,48 @@ if [[ ! "$PATH" == */home/nsk/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/home/nsk/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/home/nsk/.fzf/shell/completion.bash"
 
-# Key bindings
-# ------------
-source "/home/nsk/.fzf/shell/key-bindings.bash"
+# for displaying as long a tree as will fit in the terminal
+treefit() {
+	terminal_height=$LINES
+
+	for ((depth = 3; depth >= 1; depth--)); do
+		tree_output=$(tree -CFt -L $depth "$1" 2>/dev/null)
+		output_height=$(echo "$tree_output" | wc -l)
+
+		if ((output_height <= terminal_height)); then
+			echo "$tree_output"
+			return
+		fi
+	done
+
+	echo "$tree_output"
+}
+export -f treefit
+
+# for previewing a file or directory
+fzf_preview_f_or_d() {
+	if [ -f "$1" ]; then
+		bat --color=always --style=changes,header,grid "$1"
+	elif [ -d "$1" ]; then
+		treefit "$1"
+	else
+		echo "Not a file or directory."
+	fi
+}
+export -f fzf_preview_f_or_d
+
+# set fzf settings for file preview
+export FZF_DEFAULT_OPTS="--height=90% --layout=reverse --info=inline --bind change:first"
+export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS \
+  --preview='[ -z {q} ] && treefit . || fzf_preview_f_or_d {}' \
+  --bind shift-up:preview-page-up,shift-down:preview-page-down \
+  --bind 'ctrl-a:select-all'"
+export FZF_ALT_C_OPTS="$FZF_DEFAULT_OPTS \
+  --preview='[ -z {q} ] && treefit . || treefit {}' \
+  --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_CTRL_T_COMMAND='fd -HI --ignore-file $HOME/.dotfiles/.fzffdignore'
+export FZF_ALT_C_COMMAND='fd -HI --type d --ignore-file $HOME/.dotfiles/.fzffdignore'
+
+
+eval "$(fzf --bash)"
